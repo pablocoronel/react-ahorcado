36f73cb0ca2214821a3543e9c29ce8390c316c15
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-01-23 22:58:07 -0300

    convert artist data to an object

--- src/components/Search/Form/Form.jsx
+++ src/components/Search/Form/Form.jsx
@@ -5,49 +5,48 @@ import { LASTFM_API_KEY, SPOTIFY_TOKEN } from './../../../config/keys';
 
 // component
 const FormSearch = (props) => {
-	const [artistName, setArtistName] = useState('');
-	const [artistImage, setArtistImage] = useState('');
-	const [mbid, setMbid] = useState('');
-	const [topTracks, setTopTracks] = useState([]);
-	const [trackForPlay, setTrackForPlay] = useState('');
+	const [artist, setArtist] = useState({ name: null, image: null, id: null });
+
+	// const [topTracks, setTopTracks] = useState([]);
+	// const [trackForPlay, setTrackForPlay] = useState('');
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		searchArtist(e, setArtistName, setMbid, setArtistImage);
+		searchArtist(e, setArtist);
 	};
 
 	// al obtener el artista
 	useEffect(() => {
-		props.artist({ name: artistName, image: artistImage });
+		props.artist({ name: artist.name, image: artist.image, id: artist.id });
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [artistName, artistImage]);
+	}, [artist]);
 
 	// obtiene las canciones, mediante el codigo unico de artista
-	useEffect(() => {
-		if (mbid !== '') {
-			searchTracks(mbid, setTopTracks);
-		}
-	}, [mbid]);
+	// useEffect(() => {
+	// 	if (idArtist !== '') {
+	// 		searchTracks(idArtist, setTopTracks);
+	// 	}
+	// }, [idArtist]);
 
 	// elije el tema para el juego
-	useEffect(() => {
-		if (topTracks.length > 0) {
-			const randomTrackIndex = Math.round(
-				Math.random() * topTracks.length
-			);
+	// useEffect(() => {
+	// 	if (topTracks.length > 0) {
+	// 		const randomTrackIndex = Math.round(
+	// 			Math.random() * topTracks.length
+	// 		);
 
-			setTrackForPlay(topTracks[randomTrackIndex].name);
-		}
-	}, [topTracks]);
+	// 		setTrackForPlay(topTracks[randomTrackIndex].name);
+	// 	}
+	// }, [topTracks]);
 
 	return (
 		<Row>
 			<Col xs={12} md={{ span: 4, offset: 4 }} id="formulario">
-				<h1>Buscá una banda o músico</h1>
+				<h1>Buscá un músico</h1>
 
 				<Form onSubmit={handleSubmit}>
-					<Form.Group controlId="exampleForm.ControlInput1">
+					<Form.Group controlId="ControlInput">
 						<Form.Control
 							type="text"
 							placeholder="Rolling stones"
@@ -63,12 +62,10 @@ const FormSearch = (props) => {
 	);
 };
 
-const searchArtist = (e, setArtistName, setMbid, setArtistImage) => {
-	const artist = e.target[0].value;
+const searchArtist = (e, setArtist) => {
+	const q = e.target[0].value;
 	var url =
-		'https://api.spotify.com/v1/search?q=' +
-		artist +
-		'&type=artist&limit=5';
+		'https://api.spotify.com/v1/search?q=' + q + '&type=artist&limit=5';
 
 	fetch(url, {
 		headers: {
@@ -82,38 +79,45 @@ const searchArtist = (e, setArtistName, setMbid, setArtistImage) => {
 			console.log('Success:', response);
 
 			if (response.artists.items.length > 0) {
-				setArtistName(response.artists.items[0].name);
-				setMbid(response.artists.items[0].id);
-				setArtistImage(response.artists.items[0].images[0].url);
+				setArtist({
+					name: response.artists.items[0].name,
+					id: response.artists.items[0].id,
+					image: response.artists.items[0].images[0].url
+				});
 			} else {
-				setArtistName('No se encontraron artistas');
+				setArtist({
+					name: 'No se encontraron artistas',
+					image: null,
+					id: null
+				});
 			}
 		});
 };
 
-const searchTracks = (mbid, setTopTracks) => {
-	const API_KEY = LASTFM_API_KEY;
-	var url =
-		'http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&mbid=' +
-		mbid +
-		'&api_key=' +
-		API_KEY +
-		'&format=json';
+// mover funcion arriba y pasarla a spotify, cambiar idArtist a id
+// const searchTracks = (idArtist, setTopTracks) => {
+// 	const API_KEY = LASTFM_API_KEY;
+// 	var url =
+// 		'http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&idArtist=' +
+// 		idArtist +
+// 		'&api_key=' +
+// 		API_KEY +
+// 		'&format=json';
 
-	fetch(url, {
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	})
-		.then((res) => res.json())
-		.catch((error) => console.error('Error:', error))
-		.then((response) => {
-			// console.log('Success:', response);
+// 	fetch(url, {
+// 		headers: {
+// 			'Content-Type': 'application/json'
+// 		}
+// 	})
+// 		.then((res) => res.json())
+// 		.catch((error) => console.error('Error:', error))
+// 		.then((response) => {
+// 			// console.log('Success:', response);
 
-			if (response.toptracks) {
-				setTopTracks(response.toptracks.track);
-			}
-		});
-};
+// 			if (response.toptracks) {
+// 				setTopTracks(response.toptracks.track);
+// 			}
+// 		});
+// };
 
 export default FormSearch;
--- src/components/Search/Result/Result.jsx
+++ src/components/Search/Result/Result.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import { Row, Col, Button } from 'react-bootstrap';
+import { Row, Col, Button, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Result.css';
 
 const Result = (props) => {
-	if (props.artistName === '') {
+	console.log(props);
+	if (props.artistName === null) {
 		return null;
 	}
 
 	return (
 		<Row>
 			<Col xs={12} md={{ span: 4, offset: 4 }} id="resultados">
-				{props.artistName}
+				<Row id='title'>
+					<Col>{props.artistName}</Col>
+				</Row>
 
-				<img src={props.artistImage} alt={props.artistName} />
-				<Link to="/game">
-					<Button variant="success" renderas="button">
-						<span>Jugar!</span>
-					</Button>
-				</Link>
+				{props.artistImage !== null && (
+					<div>
+						<Row id='image'>
+							<Col>
+								<Image
+									src={props.artistImage}
+									thumbnail
+									alt={props.artistName}
+								/>
+							</Col>
+						</Row>
+
+						<Row id='play'>
+							<Col>
+								<Link to="/game">
+									<Button variant="success" renderas="button">
+										<span>Jugar!</span>
+									</Button>
+								</Link>
+							</Col>
+						</Row>
+					</div>
+				)}
 			</Col>
 		</Row>
 	);
