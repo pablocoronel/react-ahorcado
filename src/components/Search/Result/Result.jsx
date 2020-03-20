9eb06177ab77b97ec7b1edb64cf4476b28e37c2a
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-03-19 23:44:25 -0300

    get random track, changed

--- src/components/Search/Result/Result.jsx
+++ src/components/Search/Result/Result.jsx
@@ -80,13 +80,12 @@ const Result = (props) => {
 	);
 };
 
-const searchRandomTrack = (idArtist, updateInfoTrackArtist) => {
-	var url =
-		'https://api.spotify.com/v1/artists/' +
-		idArtist +
-		'/top-tracks?country=AR';
+const searchRandomTrack = async (idArtist, updateInfoTrackArtist) => {
+	var url_album =
+		'https://api.spotify.com/v1/artists/' + idArtist + '/albums';
+	let albumID = 0;
 
-	fetch(url, {
+	await fetch(url_album, {
 		headers: {
 			'Content-Type': 'application/json',
 			Authorization: 'Bearer ' + SPOTIFY_TOKEN
@@ -95,16 +94,38 @@ const searchRandomTrack = (idArtist, updateInfoTrackArtist) => {
 		.then((res) => res.json())
 		.catch((error) => console.error('Error:', error))
 		.then((response) => {
-			// console.log('Success top tracks:', response);
+			console.log('Success album:', response);
 
-			if (response.tracks.length > 0) {
-				const randomTrackIndex = Math.floor(
-					Math.random() * response.tracks.length
+			if (response.items.length > 0) {
+				const randomAlbumIndex = Math.floor(
+					Math.random() * response.items.length
 				);
 
-				const name = response.tracks[randomTrackIndex].artists[0].name;
+				albumID = response.items[randomAlbumIndex].id;
+			}
+		});
 
-				const track = response.tracks[randomTrackIndex].name
+	var url_track = 'https://api.spotify.com/v1/albums/' + albumID + '/tracks';
+
+	await fetch(url_track, {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + SPOTIFY_TOKEN
+		}
+	})
+		.then((res) => res.json())
+		.catch((error) => console.error('Error:', error))
+		.then((response) => {
+			// console.log('Success tracks:', response);
+
+			if (response.items.length > 0) {
+				const randomTrackIndex = Math.floor(
+					Math.random() * response.items.length
+				);
+
+				const name = response.items[randomTrackIndex].artists[0].name;
+
+				const track = response.items[randomTrackIndex].name
 					.toUpperCase()
 					.normalize('NFD')
 					.replace(/[\u0300-\u036f]/g, '');
